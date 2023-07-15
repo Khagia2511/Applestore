171a638f99fe5d4d1d64ba0cb3b467d719856d6d
Nguyễn Huy Hòa <52100797@student.tdtu.edu.vn>
2023-07-15 19:59:28 +0700

    First commit

---
+++ src/main/java/com/project/spring/ProjectfinalSummer2023Application.java
@@ -0,0 +1,13 @@
+package com.project.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectfinalSummer2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectfinalSummer2023Application.class, args);
+	}
+
+}
